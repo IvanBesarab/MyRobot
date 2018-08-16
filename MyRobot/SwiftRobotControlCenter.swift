96955740e1d1af01048025bcb2ab4463326f56fe
Ivan Besarab <Ivan.Besarab@RockSoft.rocks>
2018-08-16 21:12:27 +0300

    Tanin x

--- MyRobot/SwiftRobotControlCenter.swift
+++ MyRobot/SwiftRobotControlCenter.swift
@@ -10,68 +10,56 @@ import UIKit
 //all robot commands can be founded in GameViewController.h
 class SwiftRobotControlCenter: RobotControlCenter {
 
-	func l0c() {
-		move()
-		if frontIsClear {
-			doubleMove()
-			pick()
-			doubleMove()
-			turnRight()
-			move()
-			put()
-			turnLeft()
-			doubleMove()
-		}
-		else {
-			put()
-		}
-		turnRight()
-		turnLeft()
-	}
-	
-	func forLoopExample() {
-		for _ in 0..<14 {
-			put()
-			move()
-		}
-		put()
-	}
-	
 	//in this function change levelName
 	override func viewDidLoad() {
-		levelName = "L3C" // level name
-
+		levelName = "L55H" // level name
+		
 		super.viewDidLoad()
+		
 	}
 	
 	override func viewDidAppear(_ animated: Bool) {
 		
 		super.viewDidAppear(animated)
 		
-		// loop to search candy
-		while noCandyPresent { // loop to search candy
-			if frontIsClear {
-				move()
-			}
-			else {
-				break
-			}
-			put()
-			pick()
+		put()
+		turnRight()
+		while frontIsClear {
+			twoPointLeft()
 		}
 		turnRight()
+		turnRight()
+		while frontIsClear {
+			move()
+		}
+		put()
 		turnLeft()
+		while frontIsClear {
+			twoPointRight()
+		}
+		
+	}
+	
+	func twoPointRight() {
+		move()
+		turnLeft()
+		move()
+		put()
+		turnRight()
+	}
+	
+	func twoPointLeft() {
+		move()
+		turnLeft()
+		move()
+		put()
+		turnRight()
 	}
 	
 	func turnLeft() {
-		for _ in 0..<3 {
-			turnRight()
-		}
-	}
-	
-	func doubleMove() {
-		move()
-		move()
+		turnRight()
+		turnRight()
+		turnRight()
 	}
 	
 }
